776d5d42ed8da8d8bd12794eab5e25c477aa7423
Chris Kuklewicz <github@mightyreason.com>
2012-05-25 23:17:07 +0100

    Initial Commit

---
+++ Project T/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Project T
+//
+//  Created by Chris Kuklewicz on 2012/05/25.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
